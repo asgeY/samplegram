1f102133feb3e2a446af3b6acf15382816c6d1ed
Imputes <zhuzi@Bobbys-MacBook-Air.local>
2017-12-13 01:25:46 +0800

    Notifications Page.Creating notification design objects

--- Instagram/newsCell.swift
+++ Instagram/newsCell.swift
@@ -43,32 +43,25 @@ extension newsCell{
     fileprivate func setCellViewsAlignment(){
         
         // constraints
-        avaImg.translatesAutoresizingMaskIntoConstraints = false
-    usernameBtn.translatesAutoresizingMaskIntoConstraints = false
-        infoLbl.translatesAutoresizingMaskIntoConstraints = false
-        dateLbl.translatesAutoresizingMaskIntoConstraints = false
-        self.contentView.addConstraints(NSLayoutConstraint.constraints(
-            withVisualFormat: "H:|-10-[ava(40)]-10-[username]-7-[info]-7-[date]",
-            options: [], metrics: nil, views: ["ava":avaImg, "username":usernameBtn, "info":infoLbl, "date":dateLbl]))
+avaImg.translatesAutoresizingMaskIntoConstraints = false
+usernameBtn.translatesAutoresizingMaskIntoConstraints = false
+infoLbl.translatesAutoresizingMaskIntoConstraints = false
+dateLbl.translatesAutoresizingMaskIntoConstraints = false
         
-        self.contentView.addConstraints(NSLayoutConstraint.constraints(
-            withVisualFormat: "V:|-10-[ava(40)]-10-|",
-            options: [], metrics: nil, views: ["ava":avaImg]))
+self.contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-10-[ava(40)]-10-[username]-7-[info]-7-[date]",options: [], metrics: nil, views: ["ava":avaImg, "username":usernameBtn, "info":infoLbl, "date":dateLbl]))
         
-        self.contentView.addConstraints(NSLayoutConstraint.constraints(
-            withVisualFormat: "V:|-10-[username(30)]",
-            options: [], metrics: nil, views: ["username":usernameBtn]))
+self.contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-10-[ava(40)]-10-|",
+options: [], metrics: nil, views: ["ava":avaImg]))
         
-        self.contentView.addConstraints(NSLayoutConstraint.constraints(
-            withVisualFormat: "V:|-10-[info(30)]",
-            options: [], metrics: nil, views: ["info":infoLbl]))
+self.contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-10-[username(30)]",
+options: [], metrics: nil, views: ["username":usernameBtn]))
         
-        self.contentView.addConstraints(NSLayoutConstraint.constraints(
-            withVisualFormat: "V:|-10-[date(30)]",
-            options: [], metrics: nil, views: ["date":dateLbl]))
-    }
+self.contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-10-[info(30)]",options: [], metrics: nil, views: ["info":infoLbl]))
+        
+self.contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-10-[date(30)]",options: [], metrics: nil, views: ["date":dateLbl]))
+}
     
-    fileprivate func setCellViewsAttributes(){
+fileprivate func setCellViewsAttributes(){
         
         avaImg.layer.cornerRadius = avaImg.frame.size.width / 2
         avaImg.clipsToBounds = true
--- Instagram/tabbarVC.swift
+++ Instagram/tabbarVC.swift
@@ -9,13 +9,27 @@
 import UIKit
 import Parse
 
+// global variables of icons
+var icons = UIScrollView()
+var corner = UIImageView()
+var dot = UIView()
+
 class tabbarVC: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //set tab bar layer
-   setTabBarLayer()
+setTabBarLayer()
+        
+        // creare total icons
+createIcons()
+        
+        // create corner
+createCornerIcon()
+        
+        // create dot
+createDotIcon()
     }
 
     override func didReceiveMemoryWarning() {
@@ -41,5 +55,33 @@ extension tabbarVC{
         self.tabBar.barTintColor = UIColor(red: 37.0 / 255.0, green: 39.0 / 255.0, blue: 42.0 / 255.0, alpha: 1)
     }
     
+    // creare total icons
+    fileprivate func createIcons(){
+        
+        // create total icons
+        icons.frame = CGRect(x: self.view.frame.size.width / 5 * 3 + 10, y: self.view.frame.size.height - self.tabBar.frame.size.height * 2 - 3, width: 50, height: 35)
+        self.view.addSubview(icons)
+    }
+    
+    // create corner
+    fileprivate func createCornerIcon(){
+        
+        corner.frame = CGRect(x: icons.frame.origin.x, y: icons.frame.origin.y + icons.frame.size.height, width: 20, height: 14)
+        corner.center.x = icons.center.x
+        corner.image = UIImage(named: "corner.png")
+        corner.isHidden = true
+        self.view.addSubview(corner)
+    }
+    
+    // create dot
+    fileprivate func createDotIcon(){
+        
+        dot.frame = CGRect(x: self.view.frame.size.width / 5 * 3, y: self.view.frame.size.height - 5, width: 7, height: 7)
+        dot.center.x = self.view.frame.size.width / 5 * 3 + (self.view.frame.size.width / 5) / 2
+        dot.backgroundColor = UIColor(red: 251/255, green: 103/255, blue: 29/255, alpha: 1)
+        dot.layer.cornerRadius = dot.frame.size.width / 2
+        dot.isHidden = true
+        self.view.addSubview(dot)
+    }
     
 }
